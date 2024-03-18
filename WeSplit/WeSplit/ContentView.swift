d481e20b1de6c1b6443acefb7c6d4cafa6c5fa1f
Narendra Vadde <nare>
2024-03-18 22:35:31 +0530

    I am adding navigation bar along with navigation title modifier to show title in navigation bar

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,25 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form {
-            Section {
-                Text("Hello, World!")
+        NavigationStack {
+            Form {
+                Section {
+                    Text("Hello, World!")
+                }
+                
+                Section {
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                }
+                
+                Section {
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                }
             }
-            
-            Section {
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-            }
-            
-            Section {
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-            }
+            .navigationTitle("SwiftUI")
         }
     }
 }
