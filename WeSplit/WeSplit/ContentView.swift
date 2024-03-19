9bdb8170fc7c301c79654afa56483e8fe0e17b8e
Narendra Vadde <53249051+narendraiosd7@users.noreply.github.com>
2024-03-19 21:18:40 +0530

    Hiding the keyboard and completing challenge (#14)
    Co-authored-by: Narendra Vadde <nare>

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -11,15 +11,21 @@ struct ContentView: View {
     @State private var checkAmount: Double = 0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
-    var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeople)
+    var grandTotal: Double {
         let tipSelection = Double(tipPercentage)
         
         let tipValue = checkAmount / 100 * tipSelection
         let grandTotal = checkAmount + tipValue
+        
+        return grandTotal
+    }
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople)
         let amountPerPerson = grandTotal / peopleCount
         
         return amountPerPerson
@@ -31,6 +37,7 @@ struct ContentView: View {
                 Section {
                     TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "INR"))
                         .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
                     
                     Picker("Number of people:", selection: $numberOfPeople) {
                         ForEach(2..<100, id: \.self) {
@@ -41,18 +48,29 @@ struct ContentView: View {
                 
                 Section("How much do you want to tip?") {
                     Picker("Tip Percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
+                        ForEach(0..<101, id: \.self) {
                             Text($0, format: .percent)
                         }
                     }
-                    .pickerStyle(.segmented)
+                    .pickerStyle(.navigationLink)
                 }
                 
-                Section {
+                Section("Amount per person") {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "INR"))
+                }
+                
+                Section("Total Amount") {
+                    Text(grandTotal, format: .currency(code: Locale.current.currency?.identifier ?? "INR"))
                 }
             }
             .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
