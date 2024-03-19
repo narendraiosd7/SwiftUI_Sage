aa66fe4626be213f9ac2855a7a2c8799d3ed1d0e
Narendra Vadde <nare>
2024-03-19 20:46:56 +0530

    Adding a segmented control for tip percentages

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -28,6 +28,14 @@ struct ContentView: View {
                     }
                 }
                 
+                Section("How much do you want to tip?") {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                }
+                
                 Section {
                     Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "INR"))
                 }
